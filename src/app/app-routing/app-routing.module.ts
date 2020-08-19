2abf7ae3803f1a238b4e514ae94d3964ead82eb5
Lukas <lukezz3004@gmail.com>
2020-08-19 18:15:48 +0200

    routing module and app module split

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ArticlesComponent } from '../articles/articles.component';
+import { ArticleModalContainer } from '../articles/article-modal-container.component';
+
+const routes: Routes = [
+  {
+    path: 'articles',
+    children: [
+      {
+        path: '',
+        component: ArticlesComponent,
+      },
+      {
+        path: ':operation', // Route for adding articles
+        component: ArticleModalContainer,
+      },
+      {
+        path: ':operation/:id', // Route for editing articles
+        component: ArticleModalContainer,
+      },
+    ],
+  },
+  { path: '**', redirectTo: '' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,33 +12,13 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { Routes, RouterModule } from '@angular/router';
 
 import { AddArticleComponent } from './articles/add-article/add-article.component';
-import { ArticleModalContainer } from './articles/article-modal-container.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EditArticleComponent } from './articles/edit-article/edit-article.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
-const routes: Routes = [
-  {
-    path: 'articles',
-    children: [
-      {
-        path: '',
-        component: ArticlesComponent
-      },
-      {
-        path: ':operation', // Route for adding articles
-        component: ArticleModalContainer,
-      },
-      {
-        path: ':operation/:id', // Route for editing articles
-        component: ArticleModalContainer,
-      },
-    ],
-  },
-  {path: '**', redirectTo: ''}
-];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -56,7 +36,7 @@ const routes: Routes = [
     HttpClientModule,
     NgbModule,
     FontAwesomeModule,
-    RouterModule.forRoot(routes),
+    AppRoutingModule,
     AngularFireModule.initializeApp({
       apiKey: "AIzaSyBBen5fGKFHJgXdLQXD27MzBEjV_sx9gPI",
       authDomain: "dovora-shop.firebaseapp.com",
